8ad7b28d4d3ca6dcf8002df58dd46aa918d6a8fe
Wout <wout@woutmager.nl>
2018-12-13 16:00:38 +0100

    Adding conditional tags for each event item

--- PrestigeWorldWideTags.php
+++ PrestigeWorldWideTags.php
@@ -20,6 +20,20 @@ class PrestigeWorldWideTags extends Tags
     }
 
     /**
+     * The {{ prestige_world_wide:has_start_date }} tag
+     *
+     * @return string
+     */
+    public function hasStartDate()
+    {
+        if (isset($this->context['pw_start_date'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * The {{ prestige_world_wide:start_date }} tag
      *
      * @return string
@@ -60,6 +74,20 @@ class PrestigeWorldWideTags extends Tags
     }
 
     /**
+     * The {{ prestige_world_wide:has_costs }} tag
+     *
+     * @return string
+     */
+    public function hasCosts()
+    {
+        if (isset($this->context['pw_costs'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * The {{ prestige_world_wide:costs }} tag
      *
      * @return string
@@ -68,6 +96,20 @@ class PrestigeWorldWideTags extends Tags
     {
         if (isset($this->context['pw_costs'])) {
             return $this->context['pw_costs'];
+        }
+    }
+
+    /**
+     * The {{ prestige_world_wide:has_location }} tag
+     *
+     * @return string
+     */
+    public function hasLocation()
+    {
+        if (isset($this->context['pw_location'])) {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -84,6 +126,20 @@ class PrestigeWorldWideTags extends Tags
     }
 
     /**
+     * The {{ prestige_world_wide:has_url }} tag
+     *
+     * @return string
+     */
+    public function hasUrl()
+    {
+        if (isset($this->context['pw_url'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * The {{ prestige_world_wide:url }} tag
      *
      * @return string
@@ -96,6 +152,20 @@ class PrestigeWorldWideTags extends Tags
     }
 
     /**
+     * The {{ prestige_world_wide:has_organizer }} tag
+     *
+     * @return string
+     */
+    public function hasOrganizer()
+    {
+        if (isset($this->context['pw_organizer'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * The {{ prestige_world_wide:organizer }} tag
      *
      * @return string
@@ -104,6 +174,20 @@ class PrestigeWorldWideTags extends Tags
     {
         if (isset($this->context['pw_organizer'])) {
             return $this->context['pw_organizer'];
+        }
+    }
+
+    /**
+     * The {{ prestige_world_wide:has_signup }} tag
+     *
+     * @return string
+     */
+    public function hasSignup()
+    {
+        if (isset($this->context['pw_signup'])) {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -145,7 +229,7 @@ class PrestigeWorldWideTags extends Tags
     // }
 
     /**
-     * The {{ prestige_world_wide }} tag
+     * The {{ prestige_world_wide:info }} tag
      *
      * @return string|array
      */
@@ -153,28 +237,45 @@ class PrestigeWorldWideTags extends Tags
     {
         //
         $html = '<div class="pw_info">';
-        $html .= '<div class="pw_info__row">';
-        $html .= '<span class="pw_info__header">Start date:</span> <span class="pw_info__data">' . $this->startDate() . '</span>';
-        $html .= '</div>';
-        $html .= '<div class="pw_info__row">';
-        $html .= '<span class="pw_info__header">End date:</span> <span class="pw_info__data">' . $this->endDate() . '</span>';
-        $html .= '</div>';
-        $html .= '<div class="pw_info__row">';
-        $html .= '<span class="pw_info__header">Cost:</span> <span class="pw_info__data">' . $this->costs() . '</span>';
-        $html .= '</div>';
-        $html .= '<div class="pw_info__row">';
-        $html .= '<span class="pw_info__header">Location:</span> <span class="pw_info__data">' . $this->location(). '</span>';
-        $html .= '</div>';
-        $html .= '<div class="pw_info__row">';
-        $html .= '<a href="' . $this->url() .  '" class="pw_info__url">';
-        $html .= $this->organizer();
-        $html .= '</a>';
-        $html .= '</div>';
-        $html .= '<div class="pw_info__row" class="pw_info__btn">';
-        $html .= '<a href="' . $this->signup() .  '">';
-        $html .= 'Sign up';
-        $html .= '</a>';
-        $html .= '</div>';
+
+        if ($this->hasStartDate() == true) {
+            $html .= '<div class="pw_info__row">';
+            $html .= '<span class="pw_info__header">Start date:</span> <span class="pw_info__data">' . $this->startDate() . '</span>';
+            $html .= '</div>';
+        }
+        if ($this->hasEndDate() == true) {
+            $html .= '<div class="pw_info__row">';
+            $html .= '<span class="pw_info__header">End date:</span> <span class="pw_info__data">' . $this->endDate() . '</span>';
+            $html .= '</div>';
+        }
+        if ($this->hasCosts() == true) {
+            $html .= '<div class="pw_info__row">';
+            $html .= '<span class="pw_info__header">Cost:</span> <span class="pw_info__data">' . $this->costs() . '</span>';
+            $html .= '</div>';
+        }
+        if ($this->hasLocation() == true) {
+            $html .= '<div class="pw_info__row">';
+            $html .= '<span class="pw_info__header">Location:</span> <span class="pw_info__data">' . $this->location(). '</span>';
+            $html .= '</div>';
+        }
+        if ($this->hasUrl() == true) {
+            $html .= '<div class="pw_info__row">';
+            $html .= '<a href="' . $this->url() .  '" class="pw_info__url">';
+            if ($this->hasOrganizer() == true) {
+                $html .= $this->organizer();
+            } else {
+                $html .= 'External url';
+            }
+            $html .= '</a>';
+            $html .= '</div>';
+        }
+        if ($this->hasSignup() == true) {
+            $html .= '<div class="pw_info__row" class="pw_info__btn">';
+            $html .= '<a href="' . $this->signup() .  '">';
+            $html .= 'Sign up';
+            $html .= '</a>';
+            $html .= '</div>';
+        }
         $html .= '</div>';
         return $html;
     }
