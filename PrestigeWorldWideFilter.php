a835b0da3c8dfc08642b0d196eda10bf514b2905
Wout <wout@woutmager.nl>
2018-12-14 09:51:55 +0100

    Renamed the new filter to remove

--- PrestigeWorldWideFilter.php
+++ PrestigeWorldWideFilter.php
@@ -20,20 +20,20 @@ class PrestigeWorldWideFilter extends Filter
     public function filter()
     {
         //
-        $to                 = $this->getParam('to');
+        $remove             = $this->getParam('remove');
         $eventsCollection   = $this->getConfig('my_collections_field');
         $collection         = Entry::whereCollection($eventsCollection);
 
-        if ($to == 'future') {
+        if ($remove == 'future') {
             // Return future events
             return $collection->filter(function ($entry) {
-                return (new Carbon($entry->get('pw_start_date')))->gt(Carbon::now());
+                return (new Carbon($entry->get('pw_start_date')))->lt(Carbon::now());
             });
 
-        } else if ($to == 'past') {
+        } else if ($remove == 'past') {
             // Return past events
             return $collection->filter(function ($entry) {
-                return (new Carbon($entry->get('pw_start_date')))->lt(Carbon::now());
+                return (new Carbon($entry->get('pw_start_date')))->gt(Carbon::now());
             });
 
         }
