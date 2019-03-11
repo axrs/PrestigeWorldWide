196141518d5311ba5a3a5101e006d1e488de94e5
Alexander Scott <xander@cadcoder.com>
2019-03-11 23:38:44 +1000

    Allow not specifying 'remove' as a filter and support 'year' as a exclusive filter

--- PrestigeWorldWide/PrestigeWorldWideFilter.php
+++ PrestigeWorldWide/PrestigeWorldWideFilter.php
@@ -2,12 +2,11 @@
 
 namespace Statamic\Addons\PrestigeWorldWide;
 
-use Statamic\Extend\Filter;
 use Carbon\Carbon;
+use Statamic\Extend\Filter;
 
 class PrestigeWorldWideFilter extends Filter
 {
-
     /**
      * Perform filtering on a collection
      *
@@ -15,17 +14,22 @@ class PrestigeWorldWideFilter extends Filter
      */
     public function filter()
     {
-        //
         $remove = $this->getParam('remove');
+        $year = $this->getParam('year');
 
-        if ($remove == 'future') {
+        if (trim($year) != "") {
+            return $this->collection->filter(function ($entry) use ($year) {
+                if ($entry->get('pw_start_date')) {
+                    return (new Carbon($entry->get('pw_start_date')))->format('Y') == $year;
+                }
+            });
+        } else if ($remove == 'future') {
             // Return future events
             return $this->collection->filter(function ($entry) {
                 if ($entry->get('pw_start_date')) {
                     return (new Carbon($entry->get('pw_start_date')))->lt(Carbon::now());
                 }
             });
-
         } else if ($remove == 'past') {
             // Return past events
             return $this->collection->filter(function ($entry) {
@@ -33,7 +37,10 @@ class PrestigeWorldWideFilter extends Filter
                     return (new Carbon($entry->get('pw_start_date')))->gt(Carbon::now());
                 }
             });
-
+        } else {
+            return $this->collection->filter(function ($entry) {
+                return true;
+            });
         }
     }
 }
